6e0b0a933435feebc04ee25c852eb602ff5e0d5e
chenglong.ren <chenglong.ren@zhangmen.com>
2020-09-03 14:29:54 +0800

    开启配置文件

--- src/main/java/com/rcl/aspectlogspringbootstarter/aspectlog/AspectAutoConfiguration.java
+++ src/main/java/com/rcl/aspectlogspringbootstarter/aspectlog/AspectAutoConfiguration.java
@@ -5,15 +5,23 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.PriorityOrdered;
+
+import javax.annotation.Resource;
 
 /**
  * @author chenglong.ren
  * @date 2020/9/3 13:46
  * @desc
+ * 1. @EnableConfigurationProperties开启配置文件后才能注入
+ * 2. @ConditionalOnProperty 只有当aspect-log.enable=true时  才开启当前配置
  */
 @Aspect
 @EnableAspectJAutoProxy(exposeProxy = true, proxyTargetClass = true)
@@ -23,9 +31,13 @@ import org.springframework.core.PriorityOrdered;
         name = "enable",
         havingValue = "true",
         matchIfMissing = true)
+@EnableConfigurationProperties(AspectProperties.class)
 public class AspectAutoConfiguration implements PriorityOrdered {
 
     Logger log = LoggerFactory.getLogger(AspectAutoConfiguration.class);
+
+    @Autowired
+    private AspectProperties aspectProperties;
 
     @Override
     public int getOrder() {
@@ -35,6 +47,7 @@ public class AspectAutoConfiguration implements PriorityOrdered {
 
     @Around("@annotation(com.rcl.aspectlogspringbootstarter.aspectlog.AspectLog)")
     public Object isOpen(ProceedingJoinPoint thisJoinPoint) throws Throwable {
+        boolean enable = aspectProperties.isEnable();
         //执行方法名称
         String taskName = thisJoinPoint.getSignature()
                 .toString().substring(
